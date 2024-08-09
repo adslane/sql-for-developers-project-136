4a1f4ec69350c7075323ee62dae548141ecfa3ec
Slane <ad.slane@bk.ru>
2024-08-09 04:21:24 +0300

    add table in database

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-create table courses (
+create table Courses (
     id bigint primary key generated always as identity,
     name varchar(255),
     body text,
@@ -6,18 +6,18 @@ create table courses (
     updated_at timestamp
 );
 
-create table modules (
+create table Modules (
     id bigint primary key generated always as identity,
-    course_id int references courses (id),
+    course_id int references Courses (id),
     name varchar(255),
     body text,
     created_at timestamp,
     updated_at timestamp
 );
 
-create table programs (
+create table Programs (
     id bigint primary key generated always as identity,
-    module_id int references courses (id),
+    module_id int references Courses (id),
     name varchar(255),
     cost int,
     type varchar(255),
@@ -25,7 +25,7 @@ create table programs (
     updated_at timestamp
 );
 
-create table lessons (
+create table Lessons (
     id bigint primary key generated always as identity,
     name varchar(255),
     body text,
@@ -33,5 +33,23 @@ create table lessons (
     lesson_count int,
     created_at timestamp,
     updated_at timestamp,
-    course_id int references courses (id)
+    course_id int references Courses (id)
+);
+
+create table TeachingGroups (
+    id bigint primary key generated always as identity,
+    slack varchar(255),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table Users (
+    id bigint primary key generated always as identity,
+    name varchar(255),
+    email varchar(255),
+    password_hash varchar(255),
+    role varchar(50),
+    teaching_group int references TeachingGroups (id),
+    created_at timestamp,
+    updated_at timestamp
 );
